33d255d8287495b2cfc23ac860d90f4491144cdd
msrocka <michael.srocka@gmail.com>
2025-05-30 17:54:17 +0200

    initial commit

---
+++ server/src/main/java/com/greendelta/bioheating/BioheatingToolApplication.java
@@ -0,0 +1,13 @@
+package com.greendelta.bioheating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioheatingToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioheatingToolApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/greendelta/bioheating/BioheatingToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greendelta.bioheating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioheatingToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
