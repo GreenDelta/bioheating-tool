95d4de9a7510e0cc29f0b39dce023969a830aad8
msrocka <michael.srocka@gmail.com>
2025-06-05 13:44:50 +0200

    error handling with Res type

--- ui/src/api.ts
+++ ui/src/api.ts
@@ -1,6 +1,45 @@
 import { Credentials, User, Project, ProjectData } from "./model";
 
-export async function postLogin(credentials: Credentials): Promise<boolean> {
+export class Res<T> {
+
+	private constructor(
+		private readonly _val?: T,
+		private readonly _err?: string
+	) {}
+
+	static ok<T>(val: T): Res<T> {
+		return new Res(val, undefined);
+	}
+
+	static err<T>(err: string): Res<T>  {
+		return new Res(undefined as T, err)
+	}
+
+	get value(): T {
+		if (typeof this._val === "undefined") {
+			throw new Error("does not contain a value");
+		}
+		return this._val;
+	}
+
+	get isOk(): boolean {
+		return typeof this.value !== "undefined";
+	}
+
+	get hasError(): boolean {
+		return typeof this._err === "string";
+	}
+
+	get error(): string {
+		if (!this._err) {
+			throw new Error("does not contain an error");
+		}
+		return this._err;
+	}
+
+}
+
+export async function postLogin(credentials: Credentials): Promise<Res<boolean>> {
   const r = await fetch("/api/users/login", {
     method: "POST",
     headers: {
@@ -8,7 +47,11 @@ export async function postLogin(credentials: Credentials): Promise<boolean> {
     },
     body: JSON.stringify(credentials),
   });
-	return r.status === 200;
+	if (r.status === 200) {
+		return Res.ok(true);
+	}
+	const msg = await r.text();
+	return Res.err(`login failed: ${r.status} | ${msg}`);
 }
 
 export async function getCurrentUser(): Promise<User | null> {
