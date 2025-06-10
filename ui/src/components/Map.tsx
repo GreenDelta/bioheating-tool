bdb802b8b7c0b08e7e568f43a8471c8bb5ed1e31
msrocka <michael.srocka@gmail.com>
2025-06-10 18:41:14 +0200

    use plain leaflet first

--- ui/src/components/Map.tsx
+++ ui/src/components/Map.tsx
@@ -1,14 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import * as L from 'leaflet';
 
-// Fix for default markers in React Leaflet - using CDN URLs
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
-  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
-});
-
 interface MapProps {
   style?: React.CSSProperties;
   center?: [number, number];
--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -2,34 +2,35 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.tsx',
-  mode: 'production',
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, '../server/static'),
-  },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'node_modules/@picocss/pico/css/pico.min.css', to: './' },
+	entry: './src/main.tsx',
+	mode: 'production',
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, '../server/static'),
+	},
+	plugins: [
+		new CopyWebpackPlugin({
+			patterns: [
+				{ from: 'node_modules/@picocss/pico/css/pico.min.css', to: './' },
+				{ from: 'node_modules/leaflet/dist/leaflet.css', to: './' },
 				{ from: 'src/index.html', to: './' },
-      ]
-    })
-  ],
-  performance: {
-    hints: false
-  }
+			]
+		})
+	],
+	performance: {
+		hints: false
+	}
 };
