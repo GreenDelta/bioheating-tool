580308653a5c9dcd37be016a74e187db8574d11a
msrocka <michael.srocka@gmail.com>
2025-06-11 16:48:52 +0200

    this feature editing does not work!

--- ui/src/projects/project-editor.tsx
+++ ui/src/projects/project-editor.tsx
@@ -32,9 +32,75 @@ const FeaturePanel = ({ feature }: { feature: GeoFeature | null }) => {
 	if (!feature) {
 		return <></>;
 	}
+	if (!feature.properties) {
+		feature.properties = {};
+	}
+	const props = feature.properties;
 
-	return <div style={{paddingTop: 20}}>
-		<h4>{feature.properties?.name}</h4>
+	type Data = {
+		name: string,
+		height: number,
+		storeys: number,
+		heatDemand: number,
+	}
 
+	const [data, setData] = useState<Data>({
+		name: (typeof props.name === "string" ? props.name : ""),
+		height: (typeof props.height === "number" ? props.height : 0),
+		storeys: (typeof props.storeys === "number" ? props.storeys : 0),
+		heatDemand: (typeof props.heatDemand === "number" ? props.heatDemand : 0),
+	});
+
+	const handleUpdate = () => {
+		props.name = data.name;
+		props.height = data.height;
+		props.storeys = data.storeys;
+		props.heatDemand = data.heatDemand;
+	};
+
+	const handleChange = (field: keyof Data) => (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = field === 'name' ? e.target.value : parseFloat(e.target.value) || 0;
+		setData(prev => ({ ...prev, [field]: value }));
+	};
+
+	return <div style={{ paddingTop: 20 }}>
+		<h4>Building: {feature.properties?.name}</h4>
+		<fieldset>
+			<label>
+				Name
+				<input
+					value={data.name}
+					onChange={handleChange('name')} />
+			</label>
+
+			<label>
+				Height (m)
+				<input
+					type="number"
+					value={data.height}
+					onChange={handleChange('height')}
+					step="0.1" />
+			</label>
+
+			<label>
+				Storeys
+				<input
+					type="number"
+					value={data.storeys}
+					onChange={handleChange('storeys')}
+					step="1" />
+			</label>
+			<label>
+				Heat demand (kWh)
+				<input
+					type="number"
+					value={data.heatDemand}
+					onChange={handleChange('heatDemand')}
+					step="0.1" />
+			</label>
+		</fieldset>
+		<button onClick={handleUpdate} style={{ marginTop: 10 }}>
+			Update
+		</button>
 	</div>
 }
