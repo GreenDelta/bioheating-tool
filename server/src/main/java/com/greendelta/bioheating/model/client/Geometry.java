617a7d09170abf412aad6e4e195ff331280bc378
msrocka <michael.srocka@gmail.com>
2025-07-02 14:50:14 +0200

    add type hints for Geometry deserialization

--- server/src/main/java/com/greendelta/bioheating/model/client/Geometry.java
+++ server/src/main/java/com/greendelta/bioheating/model/client/Geometry.java
@@ -5,6 +5,20 @@ import java.util.List;
 
 import org.locationtech.jts.geom.Coordinate;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.greendelta.bioheating.model.client.Geometry.GeoLine;
+import com.greendelta.bioheating.model.client.Geometry.GeoPolygon;
+
+@JsonTypeInfo(
+	use = JsonTypeInfo.Id.NAME,
+	include = JsonTypeInfo.As.PROPERTY,
+	property = "type"
+)
+@JsonSubTypes({
+	@JsonSubTypes.Type(value = GeoPolygon.class, name = "Polygon"),
+	@JsonSubTypes.Type(value = GeoLine.class, name = "LineString")
+})
 public sealed interface Geometry {
 
 	static GeoPolygon polygonOf(Coordinate[] cs) {
